4a39129d4ace76577a91abfe00abdcb9f3ef11bf
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-11-10 16:46:38 +0100

    Initialisation projet

---
+++ src/main/java/fr/diginamic/rest/JavaSpringRestTp1Application.java
@@ -0,0 +1,13 @@
+package fr.diginamic.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRestTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRestTp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/rest/JavaSpringRestTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRestTp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
